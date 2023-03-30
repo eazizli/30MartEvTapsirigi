b308f2ce406a2d5e0ea07a58af696cd3300137b3
Elvin Ezizli <elvin@example.com>
2023-03-31 01:59:26 +0400

    .

---
+++ MtTapsirq/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Task 2
+        //Console.WriteLine("Eded  1-i daxil edin");
+        //string input1 = Console.ReadLine();
+        //Console.WriteLine("Eded ikini daxil edin");
+        //string input2 = Console.ReadLine();
+        //Console.WriteLine("Emeliyati daxil edin");
+        //string input3 = Console.ReadLine();
+        //Caculator(input1, input2,input3);
+
+        //Task1
+        //int[] arry = { 1, 2, 3, 4, 5, };
+        //Temp(ref arry, 12);
+        //Console.WriteLine(arry.Length);
+        
+
+        // Task2
+        //static void Caculator(string input1, string input2, string a)
+        //{
+        //    int num1;
+        //    int num2;
+
+        //    bool result1 = Int32.TryParse(input1, out num1);
+        //    bool result2 = Int32.TryParse(input2, out num2);
+
+        //    if (a == "+")
+        //    {
+        //        Console.WriteLine(num1 + num2);
+        //    }
+        //    else if (a == "-")
+        //    {
+        //        Console.WriteLine(num1 - num2);
+        //    }
+        //    else if (a == "*")
+        //    {
+        //        Console.WriteLine(num1 * num2);
+        //    }
+        //    else if (a == "/")
+        //    {
+        //        Console.WriteLine(num1 / num2);
+        //    }
+        //    else
+        //    {
+
+
+
+        //        Console.WriteLine("Zehmet olmasa duzgun emeliyat daxil edin"); 
+        //    }
+        //   while(!result1)
+        //    {
+        //        input1 = Console.ReadLine();
+        //        result1 = Int32.TryParse(input1, out num1);
+
+        //        Console.WriteLine("birinci ededi duzgun daxil edin");
+        //    }
+        //    while (!result2)
+        //    {
+        //        input2 = Console.ReadLine();
+        //        result2 = Int32.TryParse(input2, out num2);   
+        //        Console.WriteLine("Ikinci ededi duzgun daxil edin");
+        //    }
+        //    }
+        
+        // Task 2
+        //static void Temp(ref int[] arr, int num)
+        //{
+        //    int[] array = new int[arr.Length];
+
+        //    for (int i = 0; i < arr.Length ; i++)
+        //    {
+        //        array[i] = arr.Length +num;
+               
+        //    }
+        //   array = arr;  
+        //}
+
+
+    }
+}
